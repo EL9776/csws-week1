878c4f08309338a889c420a60a8db84f4a8e2913
elliotleigh2@gmail.com <elliotleigh2@gmail.com>
2022-01-25 15:00:42 +0000

    Second commit

---
+++ LAB2 Week 2.py
@@ -0,0 +1,9 @@
+NAMES = ["Elliot", "Cai", "Eben", "Cian", "Jack","Leah"]
+
+print("HI " + NAMES[0])
+print("HI " + NAMES[1])
+print("HI " + NAMES[2])
+print("HI " + NAMES[3])
+print("HI " + NAMES[4])
+print("HI " + NAMES[5])
+
